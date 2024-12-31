7b0a251ec35443c12b423a7b96367c56da5a0dc7
Agzaiyenth Ganaraj <agzaiyenth2@gmail.com>
2024-12-31 15:41:17 +0530

    Added Black Bg and re arranged

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,23 @@ const Navbar = () => {
   const { favorites } = useContext(FavoritesContext);
 
   return (
-    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center sticky top-0 z-50">
-      <Link to="/" className="text-lg font-bold">
-        Property Sale
+    <nav className="bg-black text-white p-4 flex justify-between items-center sticky top-0 z-50 shadow-md">
+      <Link
+        to="/"
+        className="text-2xl font-bold tracking-wider hover:text-oceanBlue transition-colors"
+      >
+        PropQuest
       </Link>
-      <Link to="/favorites" className="relative flex items-center space-x-2">
-        <span className="text-lg font-bold">{favorites.length}</span>
-        <OutlineHeartIcon className="w-8 h-8 text-gray-400" />
-      </Link>
+      <div className="flex items-center space-x-6">
+      
+        <Link
+          to="/favorites"
+          className="relative flex items-center space-x-2 hover:scale-105 transition-transform"
+        >
+          <span className="text-lg font-semibold">{favorites.length}</span>
+          <OutlineHeartIcon className="w-8 h-8 text-oceanBlue" />
+        </Link>
+      </div>
     </nav>
   );
 };
